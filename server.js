39ab8a656fd8151062b8cae5bbdfeaf415bff754
Hatry1337 <Hatrychannel@gmail.com>
2022-09-25 15:55:50 +0300

    add __dirname cuz we're using es modules

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import fs from "fs/promises";
 import http from "http";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const port = 8090;
 const host = "0.0.0.0";
